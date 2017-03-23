72cdfb363015ff9196bdf4cf67bfc8417cdf7a37
Vidhant Maini <vidhant_14@hotmail.com>
2017-03-23 09:24:55 +0530

    Lot of refwactoring

--- popup/settings.js
+++ popup/settings.js
@@ -14,9 +14,40 @@ If it's on a button which contains class "clear":
 
 document.addEventListener("click", (e) =>
 {
-    if (e.target.classList.contains("mode"))
+    if(e.target.checked != undefined)
     {
-        var chosenMode = e.target.textContent;
+        var chosenMode = e.target.value;
+        console.log("chosen mode is : " + chosenMode);
+
+        if(chosenMode === "flicker")
+        {
+            var modes = document.getElementsByTagName("input");
+
+            for(var i=0;i<modes.length;i++)
+            {
+                if(modes[i].value != "flicker")
+                {
+                    modes[i].style.visibility = "hidden";
+                }
+            }
+        }
+
+        var action = "apply";
+
+        if(e.target.checked === false)
+        {
+            action = "remove";
+        }
+
+        var gettingActiveTab = browser.tabs.query({ active: true, currentWindow: true });
+        gettingActiveTab.then((tabs) =>
+        {
+            browser.tabs.sendMessage(tabs[0].id, { mode: chosenMode, action:  action});
+        });
+    }
+    else if (e.target.classList.contains("mode"))
+    {
+        var chosenMode = e.target.value;
 
         //browser.tabs.executeScript(null, {
         //    file: "/content_scripts/dyslexia_working.js"
